3fc2a759c8d0d0ce39670d48bd89414888b49012
Genius Lounge Admin <info@geniuslounge.com>
2018-07-22 23:15:04 -0700

    no message

--- share2/settings.py
+++ share2/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['django_secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '*.herokuapp.com', 'share.geniuslounge.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com', 'share.geniuslounge.com']
 
 
 # Application definition
