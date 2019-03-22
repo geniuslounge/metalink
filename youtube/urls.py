75f72007e41ba27b72e3badb5c34831ac7818d2c
Genius Lounge Admin <info@geniuslounge.com>
2019-03-21 22:42:04 -0700

    Updates URLs: 	Organized so that it's easier to figure out where things were 	Added support for other channels to see feed / latest / latest image 	Corrected a typo 	Removed certain playlist vanity url slugs

--- youtube/urls.py
+++ youtube/urls.py
@@ -7,9 +7,6 @@ channel_id = os.environ['channel_id']
 urlpatterns = [
     path('giftguide', views.gift_guide, name='giftguide'),
     path('', views.home, name='home'),
-    path('clutterfree', views.redirect, {"url": "https://www.youtube.com/playlist?list=PLdr1YBmf_Da8dCv6FJ9cYWsrOxeqCVIrQ"}),
-    path('clutter-free', views.redirect,{"url": "https://www.youtube.com/playlist?list=PLdr1YBmf_Da8dCv6FJ9cYWsrOxeqCVIrQ"}),
-    path('android', views.redirect, {"url": "https://www.youtube.com/playlist?list=PLdr1YBmf_Da8do3vXAz72j8xGChHJM-3h"}),
     path('favicon.ico', views.redirect, {"url":"https://assets.geniuslounge.com/favicon.ico"}),
     path('static/favicon.ico', views.redirect, {"url":"https://assets.geniuslounge.com/favicon.ico"}),
     path('latest', views.latest_video),
@@ -17,14 +14,11 @@ urlpatterns = [
     path('contribute', views.redirect, {"url":"http://www.youtube.com/timedtext_cs_panel?tab=2&c="+channel_id}),
     path('subscribe', views.subscribe),
     path('feed', views.feed, name='feed'),
+    path('<slug:channel_id>/feed', views.feed, name='feed'),
     path('sitemap.xml',views.sitemap_render, name='sitemap'),
-    path('feed/<slug:channel_id>', views.feed, name='feed'),
-    path('<slug:channel_id>/mobile_banner', views.mobile_banner_image, name='header_image'),
     path('mobile_banner', views.mobile_banner_image, name='header_image'),
-    path('live/', views.home, name='live'),
-    path('live', views.home, name='live'),
-    path('live/<slug:video_id>', views.index, name='live'),
+    path('<slug:channel_id>/mobile_banner', views.mobile_banner_image, name='header_image'),
     path('<slug:video_id>/image', views.image_only),
-    path('<slug:video_id>', views.index, name='index'),  # Needs to be last so all the other possibilites can go first.
+    path('<slug:video_id>', views.index, name='index'),  # Needs to be last so all the other possibilities can go first.
 
 ]
