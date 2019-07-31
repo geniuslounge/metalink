26d8286aa84a578f83384ac584658310f4f0fca4
Genius Lounge Admin <info@geniuslounge.com>
2019-07-31 14:32:57 -0700

    Moved to attempt to fix the issue.

--- youtube/fb_scrape.py
+++ youtube/fb_scrape.py
@@ -4,8 +4,8 @@ import os
 
 def fb_scrape(url_to_scrape):
     payload = {
-        'scrape':'true',
         'id': url_to_scrape,
+        'scrape':'true',
         'access_token': os.environ['fb_access_token']
     }
     r = requests.post('https://graph.facebook.com/v4.0/', payload)
